e562bece3bb479d8c7fc3f1efb19b200b8bf66d1
sangeet <sangeet.verma91@gmail.com>
2016-08-30 01:31:08 +0530

    added fab in individual question

--- app/src/main/java/online/pandaapps/gre/projecteuler/Euler/SingleProblem.java
+++ app/src/main/java/online/pandaapps/gre/projecteuler/Euler/SingleProblem.java
@@ -1,13 +1,23 @@
 package online.pandaapps.gre.projecteuler.Euler;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import online.pandaapps.gre.projecteuler.Constants;
 import online.pandaapps.gre.projecteuler.R;
@@ -19,7 +29,11 @@ public class SingleProblem extends AppCompatActivity {
     int problem_id,difficulty,solved_by;
     String date_published,time_published,title,question,images;
     SQLITE3storage dbStorage;
-    TextView indi_pID,indi_pTitle, indi_question;
+    TextView indi_pID,indi_pTitle, indi_question, infoFab, pseudoFab;
+
+    FloatingActionButton fab,fab1,fab2;
+    Animation fab_open,fab_close,rotate_forward,rotate_backward,textUp,textDown;
+    Boolean isFabOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,7 @@ public class SingleProblem extends AppCompatActivity {
         setContentView(R.layout.activity_single_problem);
 
         dbStorage = new SQLITE3storage(this);
+
 
         Intent getProblemID = getIntent();
         problemID= Integer.parseInt(getProblemID.getStringExtra(Constants.col1ID));
@@ -46,7 +61,18 @@ public class SingleProblem extends AppCompatActivity {
         indi_pID = (TextView) findViewById(R.id.problemIdIndi);
         indi_pTitle = (TextView) findViewById(R.id.problemTitleIndi);
         indi_question = (TextView) findViewById(R.id.problemQuest);
-        System.out.println(question);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        infoFab = (TextView) findViewById(R.id.infoFAB);
+        pseudoFab = (TextView) findViewById(R.id.pseudoFAB);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+        textUp = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bottomup);
+        textDown = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bottomdown);
+
         indi_pID.setText("Problem "+problem_id);
         indi_pTitle.setText(title);
         Spanned spannedText;
@@ -58,9 +84,65 @@ public class SingleProblem extends AppCompatActivity {
         indi_question.setMovementMethod(new ScrollingMovementMethod());
         indi_question.setText(spannedText);
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFAB();
+            }
+        });
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFAB();
+                Toast.makeText(SingleProblem.this, "fab1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFAB();
+                Toast.makeText(SingleProblem.this, "fab2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
 
 
 
+
+    public void animateFAB(){
+
+        if(isFabOpen){
+
+            fab.startAnimation(rotate_backward);
+            fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.white)));
+            fab.setImageResource(R.drawable.panda);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            infoFab.setVisibility(View.INVISIBLE);
+            pseudoFab.setVisibility(View.INVISIBLE);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+
+        } else {
+
+            fab.startAnimation(rotate_forward);
+            fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.white)));
+            fab.setImageResource(R.drawable.pandared);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            infoFab.setVisibility(View.VISIBLE);
+            infoFab.setAnimation(textUp);
+            pseudoFab.setVisibility(View.VISIBLE);
+            pseudoFab.setAnimation(textUp);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+        }
     }
 
 
